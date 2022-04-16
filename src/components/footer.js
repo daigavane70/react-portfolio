bd4d9051b9b24f1c8e7f964e450d7296f607edf2
Vedant Daigavane <daigavane70@gmail.com>
2022-04-16 21:53:29 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="w-full flex justify-center bg-slate-100 p-5 text-center text-gray-400 space-x-2">
-      <div>Create by Vedant Daigavane</div>
+      <div>Created by Vedant Daigavane</div>
       <a
         href="https://github.com/daigavane70/daigavane70.github.io"
         target={'_blank'}

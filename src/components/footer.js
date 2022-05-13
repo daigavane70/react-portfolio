16659cc81e8e21076f3f95e6ab9f7174f62edec0
Vedant Daigavane <daigavane70@gmail.com>
2022-05-14 00:40:55 +0530

    repo link updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="w-full flex justify-center bg-slate-100 p-5 text-center text-gray-400 space-x-2">
       <div>Created by Vedant Daigavane</div>
       <a
-        href="https://github.com/daigavane70/daigavane70.github.io"
+        href="https://github.com/daigavane70/react-porfolio"
         target={'_blank'}
         rel="noreferrer"
         className="text-cyan-500"

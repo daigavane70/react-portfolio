2947b76778f83676da70f8959728f4c8f32360d4
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-05-24 18:42:05 +0530

    quote refactord

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -3,9 +3,10 @@ import { useState } from 'react/cjs/react.production.min';
 
 export default function Preloader() {
   const quotes = [
-    {quote: "Programming isn't about what you know; it's about what you can figure out.", author: "Chris Pine"},
-    {quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "Albert Einstein"},
+    // {quote: "Programming isn't about what you know; it's about what you can figure out.", author: "Chris Pine"},
+    // {quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "Albert Einstein"},
     {quote: "It's unbelievable how much you don't know about the game you've been playing all your life.", author: "Mickey Mantel"}
+    {quote: "If you know more than others, you will be the loneliest.", author: "Vedant"}
   ]
   const random = Math.floor(Math.random()*(quotes.length));
   return (

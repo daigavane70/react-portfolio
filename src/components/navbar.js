e2203eb5466e44eb441a325c6cb01183e59179ec
Vedant Daigavane <daigavane70@gmail.com>
2026-06-17 21:19:22 +0530

    Remove QR Tool link from navbar
    Route /qr still exists, just not linked from nav.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const [showNav, setShowNav] = useState(false);
-  const location = useLocation();
-  const isQRTool = location.pathname === '/qr';
 
   function closeMobileNav() {
     setShowNav(false);
@@ -31,35 +29,24 @@ export default function Navbar() {
         <i className="fa fa-bars" aria-hidden="true"></i>
       </div>
       <div className={`tabs ${showNav && 'mobile-tabs'}`}>
-        {isQRTool ? (
-          <Link onClick={closeMobileNav} to="/" className="tab-button">
-            Home
-          </Link>
-        ) : (
-          <>
-            <a onClick={closeMobileNav} href="#home" className="tab-button">
-              Home
-            </a>
-            <a onClick={closeMobileNav} href="#experience" className="tab-button">
-              Experience
-            </a>
-            <a onClick={closeMobileNav} href="#projects" className="tab-button">
-              Projects
-            </a>
-            <a onClick={closeMobileNav} href="#blogs" className="tab-button">
-              Blogs
-            </a>
-            <a onClick={closeMobileNav} href="#skills" className="tab-button">
-              Skills
-            </a>
-            <a onClick={closeMobileNav} href="#about" className="tab-button">
-              About
-            </a>
-          </>
-        )}
-        <Link onClick={closeMobileNav} to="/qr" className="tab-button" style={{ color: 'aqua' }}>
-          QR Tool
-        </Link>
+        <a onClick={closeMobileNav} href="#home" className="tab-button">
+          Home
+        </a>
+        <a onClick={closeMobileNav} href="#experience" className="tab-button">
+          Experience
+        </a>
+        <a onClick={closeMobileNav} href="#projects" className="tab-button">
+          Projects
+        </a>
+        <a onClick={closeMobileNav} href="#blogs" className="tab-button">
+          Blogs
+        </a>
+        <a onClick={closeMobileNav} href="#skills" className="tab-button">
+          Skills
+        </a>
+        <a onClick={closeMobileNav} href="#about" className="tab-button">
+          About
+        </a>
         <Link onClick={closeMobileNav} to="/vishnu" className="tab-button" style={{ color: '#00ffcc' }}>
           Vishnu
         </Link>
